6864de36f17e0a8d11a6c9a975098c1a4e9728a5
807785 <807785@SAIT224347IT.ACDM.DS.SAIT.CA>
2020-09-21 21:16:56 -0600

    Hello World working Base version with no validation at this point.

--- src/java/servlets/HelloWorldServlet.java
+++ src/java/servlets/HelloWorldServlet.java
@@ -7,7 +7,7 @@ package servlets;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
-impor t javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
