c65910b4a769dd32d8ff918d7de8811be2236248
807785 <807785@SAIT224347IT.ACDM.DS.SAIT.CA>
2020-09-28 11:07:20 -0600

    Converted application into a full MVC pattern

--- src/java/servlets/HelloWorldServlet.java
+++ src/java/servlets/HelloWorldServlet.java
@@ -10,6 +10,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import models.Person;
 
 /**
  *
@@ -27,20 +28,24 @@ public class HelloWorldServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        
         String firstname = request.getParameter("firstname");
         String lastname = request.getParameter("lastname");
-        
-        request.setAttribute("firstname", firstname);
-        request.setAttribute("lastname", lastname);
         
         // add validation
         if (firstname == null || firstname.equals("") || lastname == null || lastname.equals(""))
         {
+            request.setAttribute("firstname", firstname);
+            request.setAttribute("lastname", lastname);
             request.setAttribute("message", "Invalid entry. Please enter both your first and last name.");
             getServletContext().getRequestDispatcher("/WEB-INF/helloWorldForm.jsp")
                 .forward(request, response);
             return; // stop execution
         }
+        
+        // add Person bean (after firstname & lastname are not null)
+        Person person = new Person(firstname, lastname);
+        request.setAttribute("person", person);
 
         getServletContext().getRequestDispatcher("/WEB-INF/sayHello.jsp")
                 .forward(request, response);
